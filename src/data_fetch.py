67360f8010a3fc93219faa700ac7f12088c1f5c7
Afafe_Elhi <166409391+Afaf-elh@users.noreply.github.com>
2025-11-02 20:10:14 +0100

    Delete src directory

--- src/data_fetch.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_local_prices(csv_path: str) -> pd.DataFrame:
-    df = pd.read_csv(csv_path, parse_dates=["Date"])
-    df = df.sort_values("Date").set_index("Date")
-    return df
--- src/evaluate.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def rmse(y_true, y_pred) -> float:
-    y_true, y_pred = y_true.align(y_pred, join="inner")
-    return float(np.sqrt(((y_true - y_pred) ** 2).mean()))
